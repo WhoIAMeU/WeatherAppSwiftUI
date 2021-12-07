9926902cc85905f311b6b6bbd61833ba05deea59
WhoIAMeU <havalnij313@gmail.com>
2021-12-07 18:20:24 +0500

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Александр on 07.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
